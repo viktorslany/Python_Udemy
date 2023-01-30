370afd7c4ac78d7f3305605523843f017a982e7b
viktorslany <viktor.slany@hotmail.com>
2023-01-30 01:49:18 +0100

    Uploaded from PyCharm

--- Day_1/Day 1 Project: Band Name Generator.py
+++ Day_1/Day 1 Project: Band Name Generator.py
@@ -1,6 +1,6 @@
 #Go to: https://replit.com/@appbrewery/band-name-generator-start?v=1
 
 print('Welcome to the Band Name Generator!')
-city = input('What’s name of the city you grew up in?')
-pet = input('What’s your pet’s name?')
+city = input('What’s name of the city you grew up in?\n')
+pet = input('What’s your pet’s name?\n')
 print('Your band name could be ' + city + ' ' + pet)

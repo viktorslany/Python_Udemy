ec9c278339d980129181d22c22a0589a0cfc6b04
viktorslany <viktor.slany@hotmail.com>
2023-01-31 23:50:11 +0100

    Uploaded from PyCharm

---
+++ Day_2/Exercise 1 - Data Types.py
@@ -0,0 +1,17 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+print(int(two_digit_number[0]) + int(two_digit_number[1]))
+
+#or
+
+first_digit = two_digit_number[0]
+second_digit = two_digit_number[1]
+
+result = int(first_digit) + int(second_digit)
+
+print(result)
